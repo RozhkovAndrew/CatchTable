a62cb0116e112f3dab65db6aedecabc4e20749e2
Andrey Rozhkov <andrew93pav@gmail.com>
2022-10-20 18:57:32 +0500

    catchTable

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.comboMonthList = new System.Windows.Forms.ComboBox();
+            this.comboTeacherList = new System.Windows.Forms.ComboBox();
+            this.comboGroupList = new System.Windows.Forms.ComboBox();
+            this.dgvOutput = new System.Windows.Forms.DataGridView();
+            this.buttonTeacher = new System.Windows.Forms.Button();
+            this.buttonGroup = new System.Windows.Forms.Button();
+            this.labelMonth = new System.Windows.Forms.Label();
+            this.labelTeacher = new System.Windows.Forms.Label();
+            this.labelGroup = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DownloadButton.Location = new System.Drawing.Point(53, 24);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(196, 31);
+            this.DownloadButton.TabIndex = 0;
+            this.DownloadButton.Text = "Получить расписание";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // comboMonthList
+            // 
+            this.comboMonthList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboMonthList.FormattingEnabled = true;
+            this.comboMonthList.Location = new System.Drawing.Point(53, 90);
+            this.comboMonthList.Name = "comboMonthList";
+            this.comboMonthList.Size = new System.Drawing.Size(196, 24);
+            this.comboMonthList.TabIndex = 1;
+            // 
+            // comboTeacherList
+            // 
+            this.comboTeacherList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboTeacherList.FormattingEnabled = true;
+            this.comboTeacherList.Location = new System.Drawing.Point(277, 90);
+            this.comboTeacherList.Name = "comboTeacherList";
+            this.comboTeacherList.Size = new System.Drawing.Size(188, 24);
+            this.comboTeacherList.TabIndex = 2;
+            // 
+            // comboGroupList
+            // 
+            this.comboGroupList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboGroupList.FormattingEnabled = true;
+            this.comboGroupList.Location = new System.Drawing.Point(494, 90);
+            this.comboGroupList.Name = "comboGroupList";
+            this.comboGroupList.Size = new System.Drawing.Size(194, 24);
+            this.comboGroupList.TabIndex = 3;
+            // 
+            // dgvOutput
+            // 
+            this.dgvOutput.AllowUserToOrderColumns = true;
+            this.dgvOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOutput.Location = new System.Drawing.Point(53, 162);
+            this.dgvOutput.Name = "dgvOutput";
+            this.dgvOutput.Size = new System.Drawing.Size(635, 378);
+            this.dgvOutput.TabIndex = 4;
+            // 
+            // buttonTeacher
+            // 
+            this.buttonTeacher.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonTeacher.Location = new System.Drawing.Point(277, 24);
+            this.buttonTeacher.Name = "buttonTeacher";
+            this.buttonTeacher.Size = new System.Drawing.Size(188, 31);
+            this.buttonTeacher.TabIndex = 5;
+            this.buttonTeacher.Text = "Поиск по преподавателю";
+            this.buttonTeacher.UseVisualStyleBackColor = true;
+            this.buttonTeacher.Click += new System.EventHandler(this.buttonTeacher_Click);
+            // 
+            // buttonGroup
+            // 
+            this.buttonGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGroup.Location = new System.Drawing.Point(494, 25);
+            this.buttonGroup.Name = "buttonGroup";
+            this.buttonGroup.Size = new System.Drawing.Size(194, 30);
+            this.buttonGroup.TabIndex = 6;
+            this.buttonGroup.Text = "Поиск по группе";
+            this.buttonGroup.UseVisualStyleBackColor = true;
+            this.buttonGroup.Click += new System.EventHandler(this.buttonGroup_Click);
+            // 
+            // labelMonth
+            // 
+            this.labelMonth.AutoSize = true;
+            this.labelMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMonth.Location = new System.Drawing.Point(59, 67);
+            this.labelMonth.Name = "labelMonth";
+            this.labelMonth.Size = new System.Drawing.Size(149, 20);
+            this.labelMonth.TabIndex = 7;
+            this.labelMonth.Text = "Учебный период";
+            // 
+            // labelTeacher
+            // 
+            this.labelTeacher.AutoSize = true;
+            this.labelTeacher.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTeacher.Location = new System.Drawing.Point(284, 67);
+            this.labelTeacher.Name = "labelTeacher";
+            this.labelTeacher.Size = new System.Drawing.Size(145, 20);
+            this.labelTeacher.TabIndex = 8;
+            this.labelTeacher.Text = "Преподаватель";
+            // 
+            // labelGroup
+            // 
+            this.labelGroup.AutoSize = true;
+            this.labelGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelGroup.Location = new System.Drawing.Point(500, 67);
+            this.labelGroup.Name = "labelGroup";
+            this.labelGroup.Size = new System.Drawing.Size(67, 20);
+            this.labelGroup.TabIndex = 9;
+            this.labelGroup.Text = "Группа";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelOutput.Location = new System.Drawing.Point(60, 133);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 16);
+            this.labelOutput.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 570);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelGroup);
+            this.Controls.Add(this.labelTeacher);
+            this.Controls.Add(this.labelMonth);
+            this.Controls.Add(this.buttonGroup);
+            this.Controls.Add(this.buttonTeacher);
+            this.Controls.Add(this.dgvOutput);
+            this.Controls.Add(this.comboGroupList);
+            this.Controls.Add(this.comboTeacherList);
+            this.Controls.Add(this.comboMonthList);
+            this.Controls.Add(this.DownloadButton);
+            this.Name = "Form1";
+            this.Text = "Обозреватель расписания занятий";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.ComboBox comboMonthList;
+        private System.Windows.Forms.ComboBox comboTeacherList;
+        private System.Windows.Forms.ComboBox comboGroupList;
+        private System.Windows.Forms.DataGridView dgvOutput;
+        private System.Windows.Forms.Button buttonTeacher;
+        private System.Windows.Forms.Button buttonGroup;
+        private System.Windows.Forms.Label labelMonth;
+        private System.Windows.Forms.Label labelTeacher;
+        private System.Windows.Forms.Label labelGroup;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using System.Configuration;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dgvOutput.Visible = false;
+            dgvOutput.Rows.Clear();
+            buttonTeacher.Enabled = false;
+            buttonGroup.Enabled = false;
+            //Вырубить нижние строки для преподавателей
+            //buttonTeacher.Visible = false;
+            //comboTeacherList.Visible = false;
+            //labelTeacher.Visible = false;
+        }
+
+        //Скачать файл и обновить элементы интерфейса
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DownloadButton.Enabled = false;
+                labelOutput.Text = "Подождите, идёт загрузка и обработка файла";
+                WebClient webClient = new WebClient();
+                string path = "work.xlsx";
+                string downloadlink = "https://drive.google.com/uc?export=download&id=" + ConfigurationManager.AppSettings["DownloadlinkId"];
+                webClient.DownloadFile(downloadlink, path);
+                labelOutput.Text = "Загрузка завершена";   
+            }
+            catch
+            {
+                MessageBox.Show("Возникла проблема с загрузкой файла!");
+            }
+            finally
+            {
+                DictionarySerach();
+                DownloadButton.Enabled = true;
+            }     
+        }
+
+        //Поиск разписания занятий определенного преподавателя
+        public void SelectTeacher()
+        {
+            FileInfo ExcTable = new FileInfo("work.xlsx");
+            using (ExcelPackage ExcTblJob = new ExcelPackage(ExcTable))
+            {
+                //Ищем номер ячейки по содержимому
+                ExcelWorksheet SelectWorksheet = ExcTblJob.Workbook.Worksheets[comboMonthList.Text];
+
+                string selectTeacher = comboTeacherList.Text;
+                //Ищем номер конечной строки и столбца
+                int endColumnTable = SelectWorksheet.Cells.Last().End.Column;
+                int endRowTable = SelectWorksheet.Cells.Last().End.Row;
+
+                string dateGrid = "";
+                string timeGrid = "";
+                string groupGrid = "";
+                string subjectGrid = "";
+                string themeGrid = "";
+                string hoursGrid = "2";
+
+                DataGridViewTeacher();
+                labelOutput.Text = "Учебный план преподавателя " + selectTeacher + " за учебный период - " + comboMonthList.Text;
+
+                for (int i=3;i<= endRowTable; i++) //EndRowTable
+                {
+                    for (int j = 3; j <= endColumnTable; j++)  //EndColumnTable
+                    {
+                        if (SelectWorksheet.Cells[i, j].Text.ToString().IndexOf(selectTeacher) >= 0)
+                        {
+                            dateGrid = GetCellValueFromPossiblyMergedCell(SelectWorksheet, i, 1);
+                            if (dateGrid.IndexOf(" ") > 0)
+                            {
+                                dateGrid = dateGrid.Remove(0, dateGrid.IndexOf(" "));
+                            }
+                            timeGrid = GetCellValueFromPossiblyMergedCell(SelectWorksheet, i, 2);
+                            groupGrid = SelectWorksheet.Cells[2, j].Text.ToString();
+                            subjectGrid = SelectWorksheet.Cells[i + 1, j].Text.ToString();
+
+                            dgvOutput.Rows.Add(dateGrid, timeGrid, groupGrid, subjectGrid, themeGrid, hoursGrid);
+                        }   
+                    }
+                }
+            }
+        }
+
+        //Поиск расписания занятий для определенной группы
+        public void SelectGroup()
+        {
+            FileInfo ExcTable = new FileInfo("work.xlsx");
+            using (ExcelPackage ExcTblJob = new ExcelPackage(ExcTable))
+            {
+                ExcelWorksheet SelectWorksheet = ExcTblJob.Workbook.Worksheets[comboMonthList.Text];
+                int selectGroupColumn = 0;
+                
+                string selectGroup = comboGroupList.Text.ToString();
+                //Ищем номер конечной строки и столбца
+                int endColumnTable = SelectWorksheet.Cells.Last().End.Column;
+                int endRowTable = SelectWorksheet.Cells.Last().End.Row;
+                //Ищем номер колонки для выбранной группы
+                for (int i = 1;i<= endColumnTable;i++)
+                {
+                    if (SelectWorksheet.Cells[2, i].Text.ToString().IndexOf(selectGroup) == 0)
+                    {
+                        selectGroupColumn = i;
+                        break;
+                    }
+                }
+
+                string dateGrid = "";
+                string timeGrid = "";
+                string subjectGrid = "";
+                string placeGrid = "";
+                string teacherGrid = "";
+
+                DataGridViewGroup();
+                labelOutput.Text = "Расписание занятий группы " + selectGroup + " за учебный период - " + comboMonthList.Text;
+
+                for (int i=3;i<= endRowTable; i++)
+                {
+                    if ((SelectWorksheet.Cells[i, selectGroupColumn].Text.ToString().IndexOf(selectGroup.Substring(0,3)) < 0)&(SelectWorksheet.Cells[i, selectGroupColumn].Value!=null))
+                    {
+                        dateGrid = GetCellValueFromPossiblyMergedCell(SelectWorksheet, i, 1);
+                        if (dateGrid.IndexOf(" ")>0)
+                        {
+                            dateGrid = dateGrid.Remove(0, dateGrid.IndexOf(" "));
+                        }
+                        timeGrid = GetCellValueFromPossiblyMergedCell(SelectWorksheet, i, 2);
+                        subjectGrid = SelectWorksheet.Cells[i + 1, selectGroupColumn].Text.ToString();
+                        teacherGrid = SelectWorksheet.Cells[i, selectGroupColumn].Text.ToString();
+                        placeGrid = SelectWorksheet.Cells[i + 2, selectGroupColumn].Text.ToString();
+
+                        dgvOutput.Rows.Add(dateGrid, timeGrid, subjectGrid, teacherGrid, placeGrid);
+                        i = i + 2;
+                    }
+                }
+            }
+        }
+
+        //Поиск данных в объединенных ячейках
+        static string GetCellValueFromPossiblyMergedCell(ExcelWorksheet wks, int row, int col)
+        {
+            var cell = wks.Cells[row, col];
+            if (cell.Merge)
+            {
+                var mergedId = wks.MergedCells[row, col];
+                return wks.Cells[mergedId].First().Text.ToString();
+            }
+            else
+            {
+                return cell.Text.ToString();
+            }
+        }
+
+        //Процедура заполнения combobox (список в поле)
+        public void DictionarySerach()
+        {
+            //Заполение элементов для дальнейшего выбора
+            Dictionary<int,string> comboSourseMonth = new Dictionary<int,string>();
+            Dictionary<int, string> comboSourseTeacher = new Dictionary<int, string>();
+            Dictionary<int, string> comboSourseGroup = new Dictionary<int, string>();
+            FileInfo ExcTable = new FileInfo("work.xlsx");
+            
+            using (ExcelPackage ExcTblJob = new ExcelPackage(ExcTable))
+            {
+                //получаем количество и наименования листов в Excel файле
+                int countWorksheet = ExcTblJob.Workbook.Worksheets.Count;
+                string workSheetName = "";
+                if (countWorksheet>0)
+                {
+                    for (int i = 0; i <= countWorksheet - 1; i++)
+                    {
+                        workSheetName = ExcTblJob.Workbook.Worksheets[i].ToString().ToLower();
+                        if ((workSheetName.IndexOf("сентябрь") >= 0) | (workSheetName.IndexOf("октябрь") >= 0) | (workSheetName.IndexOf("ноябрь") >= 0) |
+                            (workSheetName.IndexOf("декабрь") >= 0) | (workSheetName.IndexOf("январь") >= 0) | (workSheetName.IndexOf("февраль") >= 0) |
+                            (workSheetName.IndexOf("март") >= 0) | (workSheetName.IndexOf("апрель") >= 0) | (workSheetName.IndexOf("май") >= 0) |
+                            (workSheetName.IndexOf("июнь") >= 0) | (workSheetName.IndexOf("июль") >= 0) | (workSheetName.IndexOf("август") >= 0))
+                        {
+                            comboSourseMonth.Add(i, workSheetName);
+                        }
+                    }
+
+                    comboMonthList.DataSource = new BindingSource(comboSourseMonth, null);
+                    comboMonthList.DisplayMember = "Value";
+                    comboMonthList.ValueMember = "Key";
+
+                    //Достаём список преподавателей и групп из листа ЗАГОТОВКИ
+                    ExcelWorksheet CommonWorksheet = ExcTblJob.Workbook.Worksheets["ЗАГОТОВКИ"];
+                    var endSheet = CommonWorksheet.Dimension.End.Row;
+
+                    for (int i = 6; i <= endSheet; i++)
+                    {
+                        if (CommonWorksheet.Cells[i, 6].Value == null)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            comboSourseTeacher.Add(i, CommonWorksheet.Cells[i, 6].Value.ToString());
+                        }
+                    }
+                    comboTeacherList.DataSource = new BindingSource(comboSourseTeacher, null);
+                    comboTeacherList.DisplayMember = "Value";
+                    comboTeacherList.ValueMember = "Key";
+
+                    for (int i = 6; i <= endSheet; i++)
+                    {
+                        if (CommonWorksheet.Cells[i, 7].Value == null)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            comboSourseGroup.Add(i, CommonWorksheet.Cells[i, 7].Value.ToString());
+                        }
+                    }
+                    comboGroupList.DataSource = new BindingSource(comboSourseGroup, null);
+                    comboGroupList.DisplayMember = "Value";
+                    comboGroupList.ValueMember = "Key";
+
+                    buttonTeacher.Enabled = true;
+                    buttonGroup.Enabled = true;                
+                }
+                else
+                {
+                    labelOutput.Text = "Нет файла для обработки";
+                }       
+            }
+        }
+
+        //Формирование таблицы для преподавателя
+        public void DataGridViewTeacher()
+        {
+            //Перед созданием таблицы удаляем предыдущие
+            int dvgColumnsCount = dgvOutput.Columns.Count - 1;
+            for (int i=0;i<=dvgColumnsCount; i++)
+            {
+                dgvOutput.Columns.Remove(dgvOutput.Columns[0]);
+            }
+            //Создание шапки для таблицы преподавателей
+            dgvOutput.Columns.Add("Date", "Дата");
+            dgvOutput.Columns.Add("Time", "Время занятия");
+            dgvOutput.Columns.Add("Group", "Группа");
+            dgvOutput.Columns.Add("Subject", "Предмет");
+            dgvOutput.Columns.Add("Theme", "Тема занятия");
+            dgvOutput.Columns.Add("Hours", "Часы");
+        }
+
+        //Формирование таблицы для студента
+        public void DataGridViewGroup()
+        {
+            //Перед созданием таблицы удаляем предыдущие
+            int dvgColumnsCount = dgvOutput.Columns.Count - 1;
+            for (int i = 0; i <= dvgColumnsCount; i++)
+            {
+                dgvOutput.Columns.Remove(dgvOutput.Columns[0]);
+            }
+            //Создание шапки для таблицы групп
+            dgvOutput.Columns.Add("Date", "Дата");
+            dgvOutput.Columns.Add("Time", "Время занятия");
+            dgvOutput.Columns.Add("Subject", "Предмет");
+            dgvOutput.Columns.Add("Teacher", "Преподаватель");
+            dgvOutput.Columns.Add("PlaceGrid", "Место");
+
+        }
+
+        //Кнопка поиска для преподавателя
+        private void buttonTeacher_Click(object sender, EventArgs e)
+        {
+            SelectTeacher();
+            dgvOutput.Visible = true;
+        }
+
+        //Кнопка поиска для студента
+        private void buttonGroup_Click(object sender, EventArgs e)
+        {
+            SelectGroup();
+            dgvOutput.Visible = true;
+        }
+    }
+}
